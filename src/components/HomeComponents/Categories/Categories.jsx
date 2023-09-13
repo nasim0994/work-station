e344779fc9d1977c20f54b426f7314cf6d1fd74c
nasim0994 <nasimuddin0994@gmail.com>
2023-09-13 11:25:46 +0600

    categoris section design

---
+++ src/components/HomeComponents/Categories/Categories.jsx
@@ -0,0 +1,232 @@
+import { Link } from "react-router-dom";
+import { AiFillStar } from "react-icons/ai";
+
+export default function Categories() {
+  return (
+    <div className="py-10 text-neutral">
+      <div className="container">
+        <h2 className="text-3xl font-semibold text-neutral">
+          Browse Jobs by category
+        </h2>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
+          <Link
+            href=""
+            className="bg-gray-50 hover:bg-gray-100 duration-300 p-4 rounded-md"
+          >
+            <div className="flex items-center gap-1">
+              <div className="w-9">
+                <img
+                  src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg"
+                  className="w-8"
+                  alt=""
+                />
+              </div>
+              <h6 className="font-medium mt-[2px]">Category Name</h6>
+            </div>
+
+            <div className="mt-4 flex items-center justify-between text-neutral-content text-sm">
+              <div className="flex gap-1 items-center">
+                <AiFillStar className="text-primary -mt-[3px]" />
+                <h6>4.5/5</h6>
+              </div>
+
+              <div>
+                <h6>99 Jobs</h6>
+              </div>
+            </div>
+          </Link>
+
+          <Link
+            href=""
+            className="bg-gray-50 hover:bg-gray-100 duration-300 p-4 rounded-md"
+          >
+            <div className="flex items-center gap-1">
+              <div className="w-9">
+                <img
+                  src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg"
+                  className="w-8"
+                  alt=""
+                />
+              </div>
+              <h6 className="font-medium mt-[2px]">Category Name</h6>
+            </div>
+
+            <div className="mt-4 flex items-center justify-between text-neutral-content text-sm">
+              <div className="flex gap-1 items-center">
+                <AiFillStar className="text-primary -mt-[3px]" />
+                <h6>4.5/5</h6>
+              </div>
+
+              <div>
+                <h6>99 Jobs</h6>
+              </div>
+            </div>
+          </Link>
+
+          <Link
+            href=""
+            className="bg-gray-50 hover:bg-gray-100 duration-300 p-4 rounded-md"
+          >
+            <div className="flex items-center gap-1">
+              <div className="w-9">
+                <img
+                  src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg"
+                  className="w-8"
+                  alt=""
+                />
+              </div>
+              <h6 className="font-medium mt-[2px]">Category Name</h6>
+            </div>
+
+            <div className="mt-4 flex items-center justify-between text-neutral-content text-sm">
+              <div className="flex gap-1 items-center">
+                <AiFillStar className="text-primary -mt-[3px]" />
+                <h6>4.5/5</h6>
+              </div>
+
+              <div>
+                <h6>99 Jobs</h6>
+              </div>
+            </div>
+          </Link>
+
+          <Link
+            href=""
+            className="bg-gray-50 hover:bg-gray-100 duration-300 p-4 rounded-md"
+          >
+            <div className="flex items-center gap-1">
+              <div className="w-9">
+                <img
+                  src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg"
+                  className="w-8"
+                  alt=""
+                />
+              </div>
+              <h6 className="font-medium mt-[2px]">Category Name</h6>
+            </div>
+
+            <div className="mt-4 flex items-center justify-between text-neutral-content text-sm">
+              <div className="flex gap-1 items-center">
+                <AiFillStar className="text-primary -mt-[3px]" />
+                <h6>4.5/5</h6>
+              </div>
+
+              <div>
+                <h6>99 Jobs</h6>
+              </div>
+            </div>
+          </Link>
+
+          <Link
+            href=""
+            className="bg-gray-50 hover:bg-gray-100 duration-300 p-4 rounded-md"
+          >
+            <div className="flex items-center gap-1">
+              <div className="w-9">
+                <img
+                  src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg"
+                  className="w-8"
+                  alt=""
+                />
+              </div>
+              <h6 className="font-medium mt-[2px]">Category Name</h6>
+            </div>
+
+            <div className="mt-4 flex items-center justify-between text-neutral-content text-sm">
+              <div className="flex gap-1 items-center">
+                <AiFillStar className="text-primary -mt-[3px]" />
+                <h6>4.5/5</h6>
+              </div>
+
+              <div>
+                <h6>99 Jobs</h6>
+              </div>
+            </div>
+          </Link>
+
+          <Link
+            href=""
+            className="bg-gray-50 hover:bg-gray-100 duration-300 p-4 rounded-md"
+          >
+            <div className="flex items-center gap-1">
+              <div className="w-9">
+                <img
+                  src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg"
+                  className="w-8"
+                  alt=""
+                />
+              </div>
+              <h6 className="font-medium mt-[2px]">Category Name</h6>
+            </div>
+
+            <div className="mt-4 flex items-center justify-between text-neutral-content text-sm">
+              <div className="flex gap-1 items-center">
+                <AiFillStar className="text-primary -mt-[3px]" />
+                <h6>4.5/5</h6>
+              </div>
+
+              <div>
+                <h6>99 Jobs</h6>
+              </div>
+            </div>
+          </Link>
+
+          <Link
+            href=""
+            className="bg-gray-50 hover:bg-gray-100 duration-300 p-4 rounded-md"
+          >
+            <div className="flex items-center gap-1">
+              <div className="w-9">
+                <img
+                  src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg"
+                  className="w-8"
+                  alt=""
+                />
+              </div>
+              <h6 className="font-medium mt-[2px]">Category Name</h6>
+            </div>
+
+            <div className="mt-4 flex items-center justify-between text-neutral-content text-sm">
+              <div className="flex gap-1 items-center">
+                <AiFillStar className="text-primary -mt-[3px]" />
+                <h6>4.5/5</h6>
+              </div>
+
+              <div>
+                <h6>99 Jobs</h6>
+              </div>
+            </div>
+          </Link>
+
+          <Link
+            href=""
+            className="bg-gray-50 hover:bg-gray-100 duration-300 p-4 rounded-md"
+          >
+            <div className="flex items-center gap-1">
+              <div className="w-9">
+                <img
+                  src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg"
+                  className="w-8"
+                  alt=""
+                />
+              </div>
+              <h6 className="font-medium mt-[2px]">Category Name</h6>
+            </div>
+
+            <div className="mt-4 flex items-center justify-between text-neutral-content text-sm">
+              <div className="flex gap-1 items-center">
+                <AiFillStar className="text-primary -mt-[3px]" />
+                <h6>4.5/5</h6>
+              </div>
+
+              <div>
+                <h6>99 Jobs</h6>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 import Hero from "../../components/HomeComponents/Hero/Hero";
+import Categories from "../../components/HomeComponents/Categories/Categories";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Categories />
     </>
   );
 }
