8c221f1d00ad2cff76d516892860fd65f37e4835
nasim0994 <nasimuddin0994@gmail.com>
2023-10-16 17:38:33 +0600

    job scleton remove

--- src/pages/Jobs/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs/Jobs.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useGetJobsQuery } from "../../../Redux/jobs/jobApi";
 
-import JobCardSkeleton from "../../../components/Skeleton/JobCardSkeleton";
 import JobsList from "../../../components/JobsComponents/JobsList";
 import JobsFilter from "../../../components/JobsComponents/JobsFilter";
 import Pagination from "../../../components/Pagination/Pagination";
@@ -24,7 +23,7 @@ export default function Jobs() {
   let content = null;
 
   if (isLoading) {
-    content = <JobCardSkeleton />;
+    content = <p>Loading...</p>;
   }
 
   if (!isLoading || isError) {
